bb65e698b31866fb9c69fbaa19503e9f3373a757
Michael Hadley <michaelwesthadley@gmail.com>
2018-01-22 14:54:45 -0600

    Add gh-pages deploy capabilities

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: "Convergence Design Lab"
   },
+  pathPrefix: "/convergencedesignlab",
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
 };
