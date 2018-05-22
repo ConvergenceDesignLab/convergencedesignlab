dfdaae8fedb33cfa9a813bf58f07ef1677f7f8e1
Michael Hadley <michaelwesthadley@gmail.com>
2018-05-22 12:32:12 -0500

    Apply padding for fixed navbar on all pages except home page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,24 +9,26 @@ import Helmet from "react-helmet";
 
 import Nav from "../components/nav";
 import Footer from "../components/footer/";
+import style from "./index.module.scss";
 
-const TemplateWrapper = ({ children, location }) => (
-  <div>
-    <Helmet
-      title="Convergence Design Lab"
-      meta={[
-        { name: "description", content: "Convergence Design Lab" },
-        { name: "keywords", content: "" }
-      ]}
-    />
-    <Nav isHomePage={location.pathname === "/"} />
-    <div>{children()}</div>
-    <Footer />
-  </div>
-);
+export default function TemplateWrapper({ children, location }) {
+  const isHomePage = location.pathname === "/";
+  return (
+    <div>
+      <Helmet
+        title="Convergence Design Lab"
+        meta={[
+          { name: "description", content: "Convergence Design Lab" },
+          { name: "keywords", content: "" }
+        ]}
+      />
+      <Nav isHomePage={isHomePage} />
+      <div className={isHomePage ? "" : style.navPadding}>{children()}</div>
+      <Footer />
+    </div>
+  );
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func
 };
-
-export default TemplateWrapper;
