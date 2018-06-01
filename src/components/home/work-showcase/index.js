155dcd5096ab8e694b629f39979bc26eaa52a535
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-01 17:15:38 -0500

    Project showcase - use live data now

--- src/components/home/work-showcase/index.js
+++ src/components/home/work-showcase/index.js
@@ -2,47 +2,28 @@ import React from "react";
 import Link from "gatsby-link";
 import Section, { Title, Description, CallToActionLink } from "../home-section/";
 import classNames from "classnames";
+import striptags from "striptags";
 import style from "./index.module.scss";
-import image1 from "../../../assets/images/projects/test-1-square.png";
-import image2 from "../../../assets/images/projects/test-2-square.png";
-import image3 from "../../../assets/images/projects/test-3-square.png";
 
-const projects = [
-  {
-    title: "Youth Expungement Mythbusters",
-    caption: "How might we empower youth to take control of their juvenile record?",
-    image: image1
-  },
-  {
-    title: "Youth Expungement Mythbusters",
-    caption: "How might we empower youth to take control of their juvenile record?",
-    image: image2
-  },
-  {
-    title: "Youth Expungement Mythbusters",
-    caption: "How might we empower youth to take control of their juvenile record?",
-    image: image3
-  }
-];
-
-export default function WorkShowcase() {
+export default function WorkShowcase({ projects }) {
   return (
     <Section title="Our Work" hasBackground={true}>
       <Title>Our Work</Title>
 
       <div className={classNames("row", style.showcase)}>
-        {projects.map(({ title, caption, image }, i) => (
-          <div key={`showcase-${title}-${i}`} className="col--xs-4">
-            <img className={style.projectImage} src={image} alt={title} />
-            <div className={style.projectDetails}>
-              <div className={style.projectTitle}>{title}</div>
-              <div className={style.projectCaption}>{caption}</div>
-              <div className={style.projectLink}>
-                <Link to="/work/">See Case Study</Link>
+        {projects &&
+          projects.map(({ slug, title, question, image }, i) => (
+            <div key={`showcase-${slug}`} className="col--xs-4">
+              <img className={style.projectImage} src={image} alt={title} />
+              <div className={style.projectDetails}>
+                <div className={style.projectTitle}>{title}</div>
+                <div className={style.projectCaption}>{striptags(question)}</div>
+                <div className={style.projectLink}>
+                  <Link to={`/work/${slug}`}>See Case Study</Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       <CallToActionLink to="/work/">See all work →</CallToActionLink>
