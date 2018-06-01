2539c448961022443cd10919ed1a7bf69a71b5f2
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-01 09:21:17 -0500

    404 page

---
+++ src/components/404/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "gatsby-link";
+import style from "./index.module.scss";
+import classNames from "classnames";
+
+export default function NotFound() {
+  return (
+    <div className={classNames("container", style.page)}>
+      <div className="section">
+        <div className={style.heading}>404</div>
+        <div className={style.subtitle}>
+          Oops! Looks like the page you are looking for doesn't exist.
+        </div>
+        <Link className={style.link} to="/">
+          Back to Home →
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,6 @@
 import React from "react";
+import NotFound from "../components/404";
 
-const NotFoundPage = props => (
-  <div className="nav-offset">
-    <div className="container">
-      <div className="section">
-        <h1>Page not found!</h1>
-        <p>You just accessed a URL that doesn&#39;t exist on the site.</p>
-      </div>
-    </div>
-  </div>
-);
+const NotFoundPage = props => <NotFound {...props} />;
 
 export default NotFoundPage;
