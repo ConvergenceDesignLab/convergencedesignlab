f2fa32cbb134da52667242bb629a98d82abaa974
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-01 17:15:13 -0500

    Home page component fetch showcase data and pass along to children

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import VideoBanner from "./video-banner/";
 import About from "./about/";
 import WorkShowcase from "./work-showcase/";
@@ -7,17 +6,27 @@ import Parnters from "./partners/";
 import ResourcesShowcase from "./resources-showcase/";
 import SocialFeeds from "./social-feeds/";
 import CallToAction from "../call-to-action/";
+import { fetchShowcase } from "../../utils/fetch-wp";
 
-export default function Home() {
-  return (
-    <div>
-      <VideoBanner />
-      <About />
-      <WorkShowcase />
-      <Parnters />
-      <ResourcesShowcase />
-      <SocialFeeds />
-      <CallToAction />
-    </div>
-  );
+export default class Home extends React.Component {
+  state = { data: null };
+
+  componentDidMount() {
+    fetchShowcase().then(json => this.setState({ data: json }));
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <VideoBanner />
+        <About />
+        <WorkShowcase projects={data ? data.projects : null} />
+        <Parnters />
+        <ResourcesShowcase resource={data ? data.resource : null} />
+        <SocialFeeds />
+        <CallToAction />
+      </div>
+    );
+  }
 }
