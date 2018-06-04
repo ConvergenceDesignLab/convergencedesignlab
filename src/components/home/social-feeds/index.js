2cc9675816355481fe1815d16a889e5329f39ecd
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-04 14:20:09 -0500

    Exxtend social integration to have follow, mention and a link to our twitter

--- src/components/home/social-feeds/index.js
+++ src/components/home/social-feeds/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Timeline, Follow, Mention } from "react-twitter-widgets";
-import Section from "../home-section/";
+import Section, { Title, Description } from "../home-section/";
 import style from "./index.module.scss";
 
 // We can hide the header and add some custom framing, potentially also adding a follow and/or
@@ -11,6 +11,16 @@ import style from "./index.module.scss";
 export default function SocialFeeds() {
   return (
     <Section>
+      <Title>Join the Conversation</Title>
+      <Description>
+        Put a bird on it <a href="https://twitter.com/convergencedlab">@ConvergenceDLab</a> iceland
+        trust fund, occupy poutine kogi sartorial XOXO la croix. Freegan portland kale chips, squid
+        four dollar toast fam photo booth.
+      </Description>
+      <div className={style.links}>
+        <Mention username="ConvergenceDLab" options={{ dnt: true, size: "large" }} />
+        <Follow username="ConvergenceDLab" options={{ count: "none", dnt: true, size: "large" }} />
+      </div>
       <div className={style.timeline}>
         <Timeline
           dataSource={{
@@ -18,7 +28,7 @@ export default function SocialFeeds() {
             screenName: "ConvergenceDLab"
           }}
           options={{
-            chrome: "nofooter", //noheader,nofooter
+            chrome: "nofooter,noheader", //noheader,nofooter
             height: "550",
             dnt: true,
             linkColor: "#ff9a21"
