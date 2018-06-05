621ae5ec6d47ce2dab90f38e5d15071ed6bc8279
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-05 07:27:24 -0500

    Add deploy to FTP via script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,13 @@
-module.exports = {
+const config = {
   siteMetadata: {
     title: "Convergence Design Lab"
   },
-  pathPrefix: "/convergencedesignlab",
   plugins: ["gatsby-plugin-react-next", "gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
 };
+
+let pathPrefix;
+if (process.env.DEPLOY_TARGET === "ftp") pathPrefix = "/new-site";
+else if (process.env.DEPLOY_TARGET === "gh-pages") pathPrefix = "/convergencedesignlab";
+if (pathPrefix) config.pathPrefix = pathPrefix;
+
+module.exports = config;
