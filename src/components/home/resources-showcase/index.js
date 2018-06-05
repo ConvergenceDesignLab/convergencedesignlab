16f9fef7a74f2a577491d56fa632d33857432321
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-05 07:50:57 -0500

    Install loading spinner in work showcase and resource showcase on home page

--- src/components/home/resources-showcase/index.js
+++ src/components/home/resources-showcase/index.js
@@ -2,7 +2,28 @@ import React from "react";
 import Link from "gatsby-link";
 import classNames from "classnames";
 import Section, { Title, Description, CallToActionLink } from "../home-section";
+import Loading from "../../loading";
 import style from "./index.module.scss";
+
+const Resource = ({ data }) => {
+  return (
+    <div className={style.resource}>
+      <div className={style.thumbnail}>
+        <img src={data.image} alt={data.title} />
+      </div>
+      <div className={style.details}>
+        <div className={style.title}>{data.title}</div>
+        <div
+          className={classNames("wordpress-content", style.description)}
+          dangerouslySetInnerHTML={{ __html: data.overview }}
+        />
+        <div className={style.link}>
+          <Link to={`resources/${data.slug}`}>Download the paper</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function ResourcesShowcase({ resource }) {
   return (
@@ -13,25 +34,7 @@ export default function ResourcesShowcase({ resource }) {
         research in connected learning...
       </Description>
 
-      <div className={style.resource}>
-        {resource && (
-          <React.Fragment>
-            <div className={style.thumbnail}>
-              <img src={resource.image} alt={resource.title} />
-            </div>
-            <div className={style.details}>
-              <div className={style.title}>{resource.title}</div>
-              <div
-                className={classNames("wordpress-content", style.description)}
-                dangerouslySetInnerHTML={{ __html: resource.overview }}
-              />
-              <div className={style.link}>
-                <Link to={`resources/${resource.slug}`}>Download the paper</Link>
-              </div>
-            </div>
-          </React.Fragment>
-        )}
-      </div>
+      {resource ? <Resource data={resource} /> : <Loading height="150px" />}
 
       <CallToActionLink to="/resources/">See all resources →</CallToActionLink>
     </Section>
--- src/components/home/work-showcase/index.js
+++ src/components/home/work-showcase/index.js
@@ -3,29 +3,33 @@ import Link from "gatsby-link";
 import Section, { Title, Description, CallToActionLink } from "../home-section/";
 import classNames from "classnames";
 import striptags from "striptags";
+import Loading from "../../loading";
 import style from "./index.module.scss";
+
+const Project = ({ slug, title, question, image }) => {
+  return (
+    <div key={`showcase-${slug}`} className={style.column}>
+      <img className={style.projectImage} src={image} alt={title} />
+      <div className={style.projectDetails}>
+        <div className={style.projectTitle}>{title}</div>
+        <div className={style.projectCaption}>{striptags(question)}</div>
+        <div className={style.projectLink}>
+          <Link to={`/work/${slug}`}>See Case Study</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Gallery = ({ projects }) => {
+  return <div className={style.showcase}>{projects.map(project => <Project {...project} />)}</div>;
+};
 
 export default function WorkShowcase({ projects }) {
   return (
     <Section title="Our Work" hasBackground={true}>
       <Title>Our Work</Title>
-
-      <div className={style.showcase}>
-        {projects &&
-          projects.map(({ slug, title, question, image }, i) => (
-            <div key={`showcase-${slug}`} className={style.column}>
-              <img className={style.projectImage} src={image} alt={title} />
-              <div className={style.projectDetails}>
-                <div className={style.projectTitle}>{title}</div>
-                <div className={style.projectCaption}>{striptags(question)}</div>
-                <div className={style.projectLink}>
-                  <Link to={`/work/${slug}`}>See Case Study</Link>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-
+      {projects ? <Gallery projects={projects} /> : <Loading height="300px" />}
       <CallToActionLink to="/work/">See all work →</CallToActionLink>
     </Section>
   );
