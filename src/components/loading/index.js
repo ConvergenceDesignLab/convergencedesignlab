a4405119deac45631b7337ed12a49bf333d7a453
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-12 12:02:47 -0500

    Bandaid: Loading use list instead of div to prevent react hydration from getting confused

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import style from "./index.module.scss";
 
-export default function Loading({ height }) {
+export default function Loading({ height, key = "loading", ...otherProps }) {
   return (
-    <div className={style.container} style={{ height }}>
-      <div className={style.circle} />
-      <div className={style.circle} />
-      <div className={style.circle} />
-    </div>
+    <ul {...otherProps} key={key} className={style.container} style={{ height }}>
+      <li className={style.circle} />
+      <li className={style.circle} />
+      <li className={style.circle} />
+    </ul>
   );
 }
