57be34d5c92ee35dd5433ed81917e14abf337e35
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-20 13:42:26 -0500

    GraphQL query on home page for all partner logos - resized and optimized

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,20 @@ import Home from "../components/home";
 const IndexPage = props => <Home {...props} />;
 
 export default IndexPage;
+
+export const query = graphql`
+  query Home {
+    partnerImages: allImageSharp(filter: { id: { glob: "**/images/partner-logos/**" } }) {
+      edges {
+        node {
+          ... on ImageSharp {
+            resize(width: 400) {
+              src
+              originalName
+            }
+          }
+        }
+      }
+    }
+  }
+`;
