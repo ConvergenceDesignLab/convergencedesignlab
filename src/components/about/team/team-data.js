ce094437026929594beaca41284b1eda2fe4883e
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-21 09:17:31 -0500

    TeamData - merge all team members together and install bios

--- src/components/about/team/team-data.js
+++ src/components/about/team/team-data.js
@@ -1,98 +1,72 @@
-const team = {
-  staff: [
-    {
-      name: "Eric Freedman, Ph.D.",
-      image: require("../../../assets/images/team/eric-freedman.jpg"),
-      title: "Dean of the School of Media Arts",
-      email: "efreedman@colum.edu",
-      twitter: "https://twitter.com/ericmfreedman",
-      linkedin: "https://www.linkedin.com/in/erfreedman"
-    },
-    {
-      name: "Mindy Faber",
-      image: require("../../../assets/images/team/mindy-faber.png"),
-      title: "Program Director",
-      email: "mfaber@colum.edu",
-      twitter: "https://twitter.com/mindyfaber",
-      linkedin: "https://www.linkedin.com/in/mindy-faber-4366025"
-    },
-    {
-      name: "Margaret Conway",
-      image: require("../../../assets/images/team/margaret-conway.jpg"),
-      title: "Program Manager",
-      email: "mconway@colum.edu",
-      twitter: "https://twitter.com/margaretkconway",
-      linkedin: "https://www.linkedin.com/in/margaret-conway-a719519"
-    },
-    {
-      name: "Michael Hadley",
-      image: require("../../../assets/images/team/mike-hadley-alt.jpg"),
-      title: "Learning Product Developer",
-      email: "mhadley@colum.edu",
-      twitter: "https://twitter.com/mikewesthad",
-      linkedin: "https://www.linkedin.com/in/michaelwesthadley"
-    }
-  ],
-  researchers: [
-    {
-      name: "Yonty Friesem",
-      image: require("../../../assets/images/team/yonty-friesem.jpg"),
-      title: "Research Associate",
-      twitter: "https://twitter.com/yonty",
-      linkedin: "https://www.linkedin.com/in/yonty"
-    },
-    {
-      name: "Natalia Smirnov",
-      image: require("../../../assets/images/team/natalia-smirnov-square.jpg"),
-      title: "Research Associate",
-      twitter: "https://twitter.com/marginatalia?lang=en",
-      linkedin: "https://www.linkedin.com/in/nataliasmirnov"
-    },
-    {
-      name: "Jabari Evans",
-      image: require("../../../assets/images/team/jabari-evans-square.jpg"),
-      title: "Research Associate",
-      twitter: "https://twitter.com/naledgeevans?lang=en",
-      linkedin: "https://www.linkedin.com/in/naledge-jabari-evans-msw-04433660/"
-    },
-    {
-      name: "Virginia Killian Lund",
-      image: require("../../../assets/images/team/virigina-lund-square.jpg"),
-      title: "Research Associate",
-      twitter: "https://twitter.com/virginiaklund",
-      linkedin: "https://www.linkedin.com/in/virginia-killian-lund-b9b393a"
-    }
-  ],
-  students: [
-    {
-      name: "Nina Sitara",
-      image: require("../../../assets/images/team/placeholder-1.jpg"),
-      title: "Student Fellow",
-      twitter: "#",
-      linkedin: "#"
-    },
-    {
-      name: "Casey Potter",
-      image: require("../../../assets/images/team/placeholder-2.jpg"),
-      title: "Student Fellow",
-      twitter: "#",
-      linkedin: "#"
-    },
-    {
-      name: "Jonathan	Miller",
-      image: require("../../../assets/images/team/placeholder-3.jpg"),
-      title: "Student Fellow",
-      twitter: "#",
-      linkedin: "#"
-    },
-    {
-      name: "Lisa	Long",
-      image: require("../../../assets/images/team/placeholder-4.jpg"),
-      title: "Student Fellow",
-      twitter: "#",
-      linkedin: "#"
-    }
-  ]
-};
+const team = [
+  {
+    name: "Eric Freedman, Ph.D.",
+    bio: require("./bios/eric-freedman"),
+    image: require("../../../assets/images/team/eric-freedman.jpg"),
+    title: "Dean of the School of Media Arts",
+    email: "efreedman@colum.edu",
+    twitter: "https://twitter.com/ericmfreedman",
+    linkedin: "https://www.linkedin.com/in/erfreedman"
+  },
+  {
+    name: "Mindy Faber",
+    bio: require("./bios/mindy-faber"),
+    image: require("../../../assets/images/team/mindy-faber.png"),
+    title: "Program Director",
+    email: "mfaber@colum.edu",
+    twitter: "https://twitter.com/mindyfaber",
+    linkedin: "https://www.linkedin.com/in/mindy-faber-4366025"
+  },
+  {
+    name: "Margaret Conway",
+    bio: require("./bios/margaret-conway"),
+    image: require("../../../assets/images/team/margaret-conway.jpg"),
+    title: "Program Manager",
+    email: "mconway@colum.edu",
+    twitter: "https://twitter.com/margaretkconway",
+    linkedin: "https://www.linkedin.com/in/margaret-conway-a719519"
+  },
+  {
+    name: "Michael Hadley",
+    bio: require("./bios/michael-hadley"),
+    image: require("../../../assets/images/team/mike-hadley-alt.jpg"),
+    title: "Learning Product Developer",
+    email: "mhadley@colum.edu",
+    twitter: "https://twitter.com/mikewesthad",
+    linkedin: "https://www.linkedin.com/in/michaelwesthadley"
+  },
+  {
+    name: "Yonty Friesem",
+    bio: require("./bios/yonty-friesem"),
+    image: require("../../../assets/images/team/yonty-friesem.jpg"),
+    title: "Research Associate",
+    twitter: "https://twitter.com/yonty",
+    linkedin: "https://www.linkedin.com/in/yonty"
+  },
+  {
+    name: "Natalia Smirnov",
+    bio: require("./bios/natalia-smirnov"),
+    image: require("../../../assets/images/team/natalia-smirnov-square.jpg"),
+    title: "Research Associate",
+    twitter: "https://twitter.com/marginatalia?lang=en",
+    linkedin: "https://www.linkedin.com/in/nataliasmirnov"
+  },
+  {
+    name: "Jabari Evans",
+    bio: require("./bios/jabari-evans"),
+    image: require("../../../assets/images/team/jabari-evans-square.jpg"),
+    title: "Research Associate",
+    twitter: "https://twitter.com/naledgeevans?lang=en",
+    linkedin: "https://www.linkedin.com/in/naledge-jabari-evans-msw-04433660/"
+  },
+  {
+    name: "Virginia Killian Lund",
+    bio: require("./bios/virginia-lund"),
+    image: require("../../../assets/images/team/virigina-lund-square.jpg"),
+    title: "Research Associate",
+    twitter: "https://twitter.com/virginiaklund",
+    linkedin: "https://www.linkedin.com/in/virginia-killian-lund-b9b393a"
+  }
+];
 
 export default team;
