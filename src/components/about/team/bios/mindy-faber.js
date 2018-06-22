45514c318490640e689d92c85cb1c37da676976a
Michael Hadley <michaelwesthadley@gmail.com>
2018-06-21 20:36:12 -0500

    Fixup Mindy's bio

--- src/components/about/team/bios/mindy-faber.js
+++ src/components/about/team/bios/mindy-faber.js
@@ -12,13 +12,9 @@ export default function MindyFaberBio() {
         the School of the Art Institute of Chicago, taught media arts at Evanston high school,
         founded Open Youth Networks, co-chaired the Chicago Youth Voices Network and organized the
         3G Summit on the Future of Girls, Gaming and Gender at the department of Interactive Arts
-        and Media at Columbia College Chicago and. Mindy holds an MFA in Art and Technology from the
+        and Media at Columbia College Chicago. Mindy holds an MFA in Art and Technology from the
         School of the Art Institute of Chicago and certificates in E-Learning and Digital Culture
         and Learning Experience Design.
-      </p>
-      <p>
-        Mindy holds an MFA in Art and Technology from the School of the Art Institute of Chicago and
-        certificates in E-Learning and Digital Culture and Learning Experience Design.
       </p>
     </div>
   );
