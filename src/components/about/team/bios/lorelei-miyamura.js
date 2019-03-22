f9d98a61e2b844467f7a30a2037129af8c4849a3
Lorelei <lorelei.miyamura@gmail.com>
2019-03-22 09:09:17 -0500

    Bio addition

---
+++ src/components/about/team/bios/lorelei-miyamura.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function EricFreedmanBio() {
+  return (
+    <div>
+      <p>
+        Lorelei is an Interactive Artist from Oahu, Hawai'i. With focus on designing interactive
+        installations amd mobile applications, her craft bridges the relation between digital and
+        physical environments. Utilization of her interdisciplinary perspective is aimed at
+        designing creative solutions to problems in the community. In her work, Lorelei has designed
+        inclusive technologies for those of ADA, hard of hearing, and queer communities. From the
+        captioning of video game dialogue to designing touchable 3d prints of paintings, she strives
+        to create a sense of belonging for everyone and all. Lorelei is currently pursuing a double
+        BA in Interaction Design and Graphic Design at Columbia College Chicago.
+      </p>
+    </div>
+  );
+}
