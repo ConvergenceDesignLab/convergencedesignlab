5c06e18a341610e8706e62b07355fb8bb93bb58b
Lorelei <lorelei.miyamura@gmail.com>
2019-10-29 12:42:14 -0500

    Jacob Watson Bio

---
+++ src/components/about/team/bios/jacob-watson.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function JacobWatsonBio() {
+  return (
+    <div>
+      <p>
+        Jacob Watson designs and facilitates experiences that move people into deeper understanding
+        of themselves and their communities. Jacob has been a researcher at Project Zero and a
+        program manager for interdisciplinary learning initiatives at Columbia College Chicago. He
+        is a founding member of the FYI Performance Company at the Illinois Caucus for Adolescent
+        Health, which creates participatory theatre experiences that activate the creative potential
+        of sexuality education. He has directed and designed performances with Lyric Opera of
+        Chicago, Redmoon, Theatre Unspeakable, FYI, Piven Theatre Workshop, Chicago Home Theatre
+        Festival, and Erasing the Distance. His work has been supported by the Chicago Department of
+        Cultural Affairs &amp; Special Events, Swarm Artist Residency, and the Faber Residency in
+        Olot, Spain. He has led trainings and presentations nationally at convenings including
+        SXSWedu, the U.S. Department of Education, and the Network of Ensemble Theaters. Jacob has a
+        certificate in Human-Centered Design from IDEO.org and has trained with DirectorsLabChicago,
+        Santa Cruz Museum of Art + History, and Sojourn Theatre/Center for Performance and Civic
+        Practice. His writing can be found online at Education Week and Medium.com. Jacob holds an
+        Ed.M. in Arts in Education from Harvard University and a B.A. in Theatre from Northwestern
+        University.
+      </p>
+    </div>
+  );
+}
