14d854eb29b509a43e13ae06ea4920b239f8cf1e
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2019-11-04 12:49:28 -0600

    Adding in Serene's bio

---
+++ src/components/about/team/bios/serene-alene.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function SereneAleneBio() {
+  return (
+    <div>
+      <p>
+        Serene Arena is a designer and visual strategist currently working towards her Masters in
+        Civic Media at Columbia College Chicago. She creates materials and language that focus on
+        elevating citizen engagement through intentional interactions between individuals and larger
+        social systems. Serene hails from Detroit and has a BFA and BGS from the University of
+        Michigan, since then having also taught design fundamentals within the framework of critical
+        visual thinking at the College for Creative Studies. She can usually be found eating
+        cookies, talking about the ways in which we perceive reality, and losing her glasses on top
+        of her head.
+      </p>
+    </div>
+  );
+}
